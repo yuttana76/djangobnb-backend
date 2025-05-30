c1c23daebaeaa475389870ac16edad16b0f44e4b
MPAM DEV03 <mpamdev03@MPAMs-Air.true.true>
2025-05-31 04:46:21 +0700

    Finish on chat

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -1,5 +1,8 @@
 import os
+
+from datetime import timedelta
 from pathlib import Path
+# from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,21 +25,104 @@ if DEBUG:
 else:
     ALLOWED_HOSTS = ["64.226.81.32"]
 
+AUTH_USER_MODEL = 'useraccount.User'
+
+SITE_ID = 1
+
+if DEBUG:
+    WEBSITE_URL = 'http://localhost:8000'
+else:
+    WEBSITE_URL = 'http://64.226.81.32:1337'
+
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKEN": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": True,
+    "SIGNING_KEY": "acomplexkey",
+    "ALOGRIGTHM": "HS512",
+}
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_VERIFICATION = None
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://64.226.81.32',
+    'http://64.226.81.32:1337'
+]
+
+CORS_ORIGINS_WHITELIST = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://64.226.81.32',
+    'http://64.226.81.32:1337'
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+REST_AUTH = {
+    "USE_JWT": True,
+    "JWT_AUTH_HTTPONLY": False
+}
 
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Rest api
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+
+    # Auth
+    'allauth',
+    'allauth.account',
+
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+
+    # Cors allow from front end.
+    'corsheaders',
+
+    # Custom app
+    'chat',
+    'property',
+    'useraccount',
+
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware', # add this
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -62,6 +148,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "djangobnb_backend.wsgi.application"
+ASGI_APPLICATION = 'djangobnb_backend.asgi.application'
 
 
 # Database
@@ -112,7 +199,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ djangobnb_backend/property/api.py
@@ -0,0 +1,137 @@
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework_simplejwt.tokens import AccessToken
+from .forms import PropertyForm
+from .models import Property, Reservation
+from .serializers import PropertiesDetailSerializer, PropertiesListSerializer, ReservationsListSerializer
+from useraccount.models import User
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_list(request):
+
+    #
+    # Auth
+
+    try:
+        token = request.META['HTTP_AUTHORIZATION'].split('Bearer ')[1]
+        token = AccessToken(token)
+        user_id = token.payload['user_id']
+        user = User.objects.get(pk=user_id)
+    except Exception as e:
+        user = None
+
+    # print(user)
+    #
+    #
+
+    favorites = []
+    properties = Property.objects.all()
+
+    # Fileter
+    is_favorites = request.GET.get('is_favorites', '')
+    landlord_id = request.GET.get('landlord_id', '')
+
+    if landlord_id:
+        properties = properties.filter(landlord_id=landlord_id)
+
+    if is_favorites:
+        properties = properties.filter(favorited__in=[user])
+
+    # Favorites
+        
+    if user:
+        for property in properties:
+            if user in property.favorited.all():
+                favorites.append(property.id)
+
+    # print(favorites)
+    # Fileter
+    
+    serializer = PropertiesListSerializer(properties, many=True)
+
+    return JsonResponse({
+        'data': serializer.data,
+        'favorites': favorites
+    })
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_detail(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    serializer = PropertiesDetailSerializer(property, many=False)
+
+    return JsonResponse(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def property_reservations(request, pk):
+    property = Property.objects.get(pk=pk)
+    reservations = property.reservations.all()
+
+    serializer = ReservationsListSerializer(reservations, many=True)
+
+    return JsonResponse(serializer.data, safe=False)
+
+
+
+@api_view(['POST', 'FILES'])
+def create_property(request):
+    form = PropertyForm(request.POST, request.FILES)
+
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+
+        return JsonResponse({'success': True})
+    else:
+        print('error', form.errors, form.non_field_errors)
+        return JsonResponse({'errors': form.errors.as_json()}, status=400)
+
+
+@api_view(['POST'])
+def book_property(request, pk):
+    try:
+        start_date = request.POST.get('start_date', '')
+        end_date = request.POST.get('end_date', '')
+        number_of_nights = request.POST.get('number_of_nights', '')
+        total_price = request.POST.get('total_price', '')
+        guests = request.POST.get('guests', '')
+
+        property = Property.objects.get(pk=pk)
+
+        Reservation.objects.create(
+            property=property,
+            start_date=start_date,
+            end_date=end_date,
+            number_of_nights=number_of_nights,
+            total_price=total_price,
+            guests=guests,
+            created_by=request.user
+        )
+
+        return JsonResponse({'success': True})
+    except Exception as e:
+        print('Error', e)
+
+        return JsonResponse({'success': False})
+
+@api_view(['POST'])
+def toggle_favorite(request, pk):
+    property = Property.objects.get(pk=pk)
+
+    if request.user in property.favorited.all():
+        property.favorited.remove(request.user)
+
+        return JsonResponse({'is_favorite': False})
+    else:
+        property.favorited.add(request.user)
+
+        return JsonResponse({'is_favorite': True})
